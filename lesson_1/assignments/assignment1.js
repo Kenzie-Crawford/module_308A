0b80acb637f592e63918636984e03193d5edf01c
Kenzie-Crawford <kenzie.scrawford13@gmail.com>
2026-02-10 13:04:43 -0500

    corrections

--- lesson_1/assignments/assignment1.js
+++ lesson_1/assignments/assignment1.js
@@ -1,37 +1,37 @@
-function countUp(n) {
-    if (n === 0) return "done!";
-    console.log(n);
-    return countUp(n + 1)
-    }  
+// function countUp(n) {
+//     if (n === 15000) return "done!";
+//     console.log(n);
+//     return countUp(n + 1)
+//     }  
 
-    try{
-    console.log(countUp(0));
-    } catch (e) {
-    console.log("Stack overflow!", e.message);
+//     try{
+//     console.log(countUp(0));
+//     } catch (e) {
+//     console.log("Stack overflow!", e.message);
+//     }
+
+
+
+
+
+
+
+
+ function trampoline(fn) {
+    let result = fn();
+    while (typeof result === 'function') {
+        result = result();
+    }
+    return result;
+}
+
+function countUp(n) {
+    if (n === 15000) return "done!"; 
+     console.log(n);
+    
+        return () => countUp(n + 1);
     }
 
-
-
-
-
-
-
-
-//  function trampoline(fn) {
-//     let result = fn();
-//     while (typeof result === 'function') {
-//         result = result();
-//     }
-//     return result;
-// }
-
-// function countUp(n) {
-//     if (n === 15000) return "done!"; 
-//      console.log(n);
-    
-//         return () => countUp(n + 1);
-//     }
-
-// const result = trampoline(() => countUp(0));
-// console.log('No stack overflow!', result); 
+const result = trampoline(() => countUp(0));
+console.log('No stack overflow!', result); 
 
