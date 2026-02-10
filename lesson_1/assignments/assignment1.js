d0090d1a1d04db77b14ed691860d8b85da26ad3e
Kenzie-Crawford <kenzie.scrawford13@gmail.com>
2026-02-10 11:23:35 -0500

    part 1 and 2 I think?

--- lesson_1/assignments/assignment1.js
+++ lesson_1/assignments/assignment1.js
@@ -1,19 +1,37 @@
-function trampoline(fn) {
-    let result = fn();
-    while (typeof result === 'function') {
-        result = result();
-    }
-    return result;
-}
-
 function countUp(n) {
-    
-        
-     console.log(n);
-    return function () {
-        return countUp(n + 1);
+    if (n === 0) return "done!";
+    console.log(n);
+    return countUp(n + 1)
+    }  
+
+    try{
+    console.log(countUp(0));
+    } catch (e) {
+    console.log("Stack overflow!", e.message);
     }
-}
-const result = trampoline(() => countUp(0));
-console.log('No stack overflow!', result);
+
+
+
+
+
+
+
+
+//  function trampoline(fn) {
+//     let result = fn();
+//     while (typeof result === 'function') {
+//         result = result();
+//     }
+//     return result;
+// }
+
+// function countUp(n) {
+//     if (n === 15000) return "done!"; 
+//      console.log(n);
+    
+//         return () => countUp(n + 1);
+//     }
+
+// const result = trampoline(() => countUp(0));
+// console.log('No stack overflow!', result); 
 
