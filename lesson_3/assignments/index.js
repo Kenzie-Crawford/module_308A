1220ff86ad1507acb8fa2ae0c51aef6b0492c876
Kenzie-Crawford <kenzie.scrawford13@gmail.com>
2026-02-12 11:03:24 -0500

    assingment 3 object

--- lesson_3/assignments/index.js
+++ lesson_3/assignments/index.js
@@ -8,10 +8,39 @@ dblocation.then((db) => {
     console.log(`The user is in${db}`);
 });
 
-function getUserData(id) {
+async function getUserData(id) {
   const dbs = {
     db1: db1,
     db2: db2,
     db3: db3
   };
+  try {
+    if(typeof id !== "number") throw new Error("Invalid Input -- Not a Number");
+  }
+    catch (error) {
+        console.error(error);
+        return;
+    }
+
+  const dbName = await central(id);
+  const [basicInfo, personalInfo] = await Promise.all([
+    dbs[dbName](id),
+    vault(id)
+  ]);  
+  
+  return {
+    id,
+    name: personalInfo.name,
+    username: basicInfo.username,
+    email: personalInfo.email,
+    address: personalInfo.address,
+    phone: personalInfo.phone,
+    website: basicInfo.website,
+    company: basicInfo.company  
+
+  };
+  
 }
+
+
+
